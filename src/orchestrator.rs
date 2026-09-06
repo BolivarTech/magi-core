e295e2d728c0b864c7b9c10391717cc2dff1f284
Julian Bolivar <jbolivarg@gmail.com>
2026-09-06 09:09:09 -0400

    chore: stamp MS1 file headers with the release version

--- src/orchestrator.rs
+++ src/orchestrator.rs
@@ -1,6 +1,6 @@
 // Author: Julian Bolivar
-// Version: 4.0.0
-// Date: 2026-08-23
+// Version: 4.1.0
+// Date: 2026-09-06
 
 use std::collections::{BTreeMap, BTreeSet};
 use std::path::PathBuf;
--- src/providers/provider_url.rs
+++ src/providers/provider_url.rs
@@ -1,6 +1,6 @@
 // Author: Julian Bolivar
-// Version: 4.0.0
-// Date: 2026-08-23
+// Version: 4.1.0
+// Date: 2026-09-06
 
 //! Authority over a provider's URL.
 //!
